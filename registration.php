757b66e508b8d4abca42beb113bee472a1378c33
reesmcivor <reesmcivor@hotmail.com>
2023-09-08 14:22:36 +0100

    Auto

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Register the theme
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/ReesMcIvor/HyvaPurePackages',
+    __DIR__
+);
